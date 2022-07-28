42362cb8c3b9d9ee3113883d0d7a0c60e04e100d
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-07-28 22:58:30 +0100

    Fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,12 +1,12 @@
 module MyEnumerable
-  def all?
-    each { |n| return false unless yield n }
-    true
-  end
-  
   def any?
     each { |n| return true if yield n }
     false
+  end
+
+  def all?
+    each { |n| return false unless yield n }
+    true
   end
 
   def filter
